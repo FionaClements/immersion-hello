19a2884a189cde3299934f18c9f641f2bd8dc9f0
Fiona Clements <Fiona.Clements@dnvgl.com>
2018-07-24 17:06:57 +0100

    Modify hello to use greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,7 @@
+require 'greeter'
 # Default is "World"
 # Author: Fiona Clements, kelper@talk21.com
 name = ARGV.first || "World"
-puts "Hello, #{name}!"
+
+greeter = Greeter.new(name)
+puts greeter.greet
